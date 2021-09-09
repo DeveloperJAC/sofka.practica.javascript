a76bf1adcfa5cc181bc90e41d4d925d3f71b8e2b
DeveloperJAC <connectionwweb@gmail.com>
2021-09-09 03:17:34 -0500

    Se creo la funcion para la colision de la pelota con una barra que recibe como parámetro

--- main.js
+++ main.js
@@ -30,7 +30,7 @@
         this.direction = -1;
         this.bounce_angle = 0;
         this.max_bounce_angle = Math.PI / 12;
-        this.speed = 6;
+        this.speed = 4;
         this.kind = "circle";
         board.setBall(this);
 
@@ -40,7 +40,28 @@
         move: function () {
             this.x += this.speed_x * this.direction;
             this.y += this.speed_y;
+        },
+        get width(){
+            return this.radius * 2;
+
+        },
+        get height(){
+            return this.radius *2;
+        },
+        collision: function(bar){
+            //Reacciona a la colisión con una barra que recibe como parámetro
+            let relative_intersect_y = bar.y + bar.height / 2 - this.y;
+
+            let normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > this.board.width / 2) this.direction = -1;
+            else this.direction = 1;
         }
+
     }
 })();
 
@@ -96,15 +117,49 @@
                 draw(this.ctx,el);
             };
         },
+
+        check_collisions: function(){
+            for(var i = Things.length - 1; i >= 0; i--){
+               var bar = this.board.bar[i];
+               if(hit(bar, this.board.ball)){
+                   this.board.ball.collision(bar);
+               }
+            };   
+        },
+
         play: function(){
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
             
         }
+       
     }
+    function hit(a, b) {
+        //Revisa si a colisiona con b
+        let hit = false;
+        //Colsiones horizontales
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            //Colisiones verticales
+            if (b.y + b.height >= a.y && b.y < a.y + a.height) hit = true;
+        }
+        //Colisión de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height) hit = true;
+        }
+        //Colisión b con a
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height) hit = true;
+        }
+
+        return hit;
+
+    }
+
+
     function draw(ctx,element){
         if(element !== null && element.hasOwnProperty("kind")){
             switch(element.kind){
